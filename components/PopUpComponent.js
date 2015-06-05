a5562404cdc623fcd3d0f7e19c0987b12d497f6f
jarusified <jarus3001@gmail.com>
2015-06-06 04:56:06 +0530

    pop up component

--- components/PopUpComponent.js
+++ components/PopUpComponent.js
@@ -2,16 +2,27 @@ import React from 'react';
 
 export default React.createClass({
 
-	handleClick(e) {
-		let name = this.ref.name.getDOMNode(value);
+	getInitialState() {
+		return {
+			message : ''
+		}
+	},
+
+	handleClick(event) {
+		let name = this.refs.name.props.value;
 		console.log(name);
 	},
+
+	handleChange(event) {
+    	this.setState({message: event.target.value});
+			console.log(this.state.message);
+  },
 
 	render() {
 		return <div className="popup">
 			Write Your Name?
-			<input ref="name" type="text" value="" />
-			<button className="button" onClick={this.handleClick}>Submit</button>
+			<input ref="name" type="text" onChange={this.handleChange} value={this.state.message} />
+			<button className="button" onClick={this.handleClick} >Submit</button>
 		</div>
 	}
 });
