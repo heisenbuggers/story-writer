ef995a16c941681f6d208d044cd6f3b3ab245733
Vignesh Shanmugam <vignesh.shanmugam@flipkart.com>
2015-06-05 22:31:52 +0530

    Msg

--- static/app.js
+++ static/app.js
@@ -1,14 +1,15 @@
-var socket = io.connect('http://localhost:3000');
+$(function() {
+	var socket = io.connect('http://localhost:3000');
 
-var inputEle = $('story');
+	var inputEle = $('#story');
 
-inputEle.keyup(function() {
-  	console.log(value);
-  	socket.emit('onType', value);
-});
+	inputEle.keyup(function() {
+	  	var value = inputEle.val();
+	  	socket.emit('onType', value);
+	});
 
-inputEle.keydown(function() {
-  	var value = this.value;
-  	console.localhostog(value);
-  	socket.emit('onType', value);
+	inputEle.keydown(function() {
+	  	var value = inputEle.val();
+	  	socket.emit('onType', value);
+	});
 });
