6dcfda26d2d8eea74bd44780c80df6763f7eff95
Vignesh Shanmugam <vignesh.shanmugam@flipkart.com>
2015-06-05 23:58:29 +0530

    Story Comp

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
+const socket = io.connect('http://localhost:3000');
+
 let StoryComponent =  React.createClass({
+
+	getInitialState() {
+		return {
+			message : ''
+		}
+	},
+
+	handleKeyUp() {
+    	this.setState({message: event.target.value});
+  	},
+
 	render() {
 		return <div>
 			<h2>Start Making a Story</h2>
 			<div className="story">
-
+				<input className="story-text" type="text" value={this.state.message}/>
 			</div>
 		</div>;
 	}
 });
 
-React.render(<StoryComponent />,document.getElementById("container"));
+React.render(<StoryComponent socket={socket} />,document.getElementById("container"));
