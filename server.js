2facaf26fee9ed90cd585e06a5bb0d1a3f6edfa7
Vignesh Shanmugam <vignesh.shanmugam@flipkart.com>
2015-06-05 22:22:19 +0530

    Intergrate Socket.io

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	port = process.env.PORT || 3000,
-	socket = require('socket.io')(server);
+	io = require('socket.io')(server);
 
 app.use(express.static(__dirname));
 
 server.listen(port,function(){
 	console.log('Express listening at ' + port);
 });
+
+io.on('connection', function (socket) {
+	socket.on('onType', function (data) {
+   	 	console.log(data);
+  	});
+})
